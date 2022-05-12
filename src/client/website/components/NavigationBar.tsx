4689879215e71300f1b236b95f9767642dad8f79
Phoenix <goldenphoenix201@gmail.com>
2022-05-12 18:55:50 -0400

    Started some work

--- src/client/website/components/NavigationBar.tsx
+++ src/client/website/components/NavigationBar.tsx
@@ -1,44 +1,50 @@
-import '../styles/NavigationBar.css'
 import {Link} from "react-scroll";
 
 export const NavigationBar = () => {
     return (
-        <nav>
-            <ul>
-                <li>
-                    <Link
-                        activeClass="active"
-                        to="section1"
-                        spy={true}
-                        smooth={true}
-                        offset={-70}
-                        duration={500}
-                    >About</Link>
-                </li>
+        <header>
+            <nav className={"navList"} style={{
+                display: "flex",
+                justifyContent: "space-around"
+            }}>
+                <ul style={{
+                    listStyle: "none",
+                    float: "left"
+                }}>
+                    <li>
+                        <Link
+                            activeClass="active"
+                            to="section1"
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration={500}
+                        >About</Link>
+                    </li>
 
-                <li>
-                    <Link
-                        activeClass="active"
-                        to="section1"
-                        spy={true}
-                        smooth={true}
-                        offset={-70}
-                        duration={500}
-                    >Projects</Link>
-                </li>
+                    <li>
+                        <Link
+                            activeClass="active"
+                            to="section1"
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration={500}
+                        >Projects</Link>
+                    </li>
 
-
-                <li>
-                    <Link
-                        activeClass="active"
-                        to="section1"
-                        spy={true}
-                        smooth={true}
-                        offset={-70}
-                        duration={500}
-                    >Skills</Link>
-                </li>
-            </ul>
-        </nav>
+                    <li>
+                        <Link
+                            activeClass="active"
+                            to="section1"
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration={500}
+                        >Skills</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
     )
 }
---
+++ src/client/website/pages/MainPage.tsx
@@ -0,0 +1,17 @@
+import '../styles/MainPage.css'
+import {NavigationBar} from "../components/NavigationBar";
+
+export const MainPage = () => {
+    return (
+        <div className={"mainPage"}>
+            {/*<NavigationBar/>*/}
+            <h1 id={"name"} className={"text"}>Hi, I'm Jay.</h1>
+            <h1 id={"position"} className={"text"}>A Full-Stack Developer</h1>
+            <p id={"description"} className={"text"}>
+                I'm a software engineer from Ontario Canada and I have over 5 years of experience in designing and
+                developing unique and innovative software applications. I can do everything from modding games to
+                creating fullstack applications.
+            </p>
+        </div>
+    )
+}
