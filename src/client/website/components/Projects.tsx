013bb06db8f1b7056cd940b53ed2996d3007d38d
Phoenix <goldenphoenix201@gmail.com>
2022-05-13 13:41:30 -0400

    Added a new project

--- src/client/website/components/Projects.tsx
+++ src/client/website/components/Projects.tsx
@@ -11,22 +11,29 @@ export const Projects = () => {
             stack: ['Java', 'Netty', 'Log4J']
         },
         {
-            name: "DayM Afterlife",
-            source: "somewhere",
-            description: "taaa daaaa",
-            stack: []
+            name: "Voxel API",
+            source: "https://github.com/PhoenixxFTW",
+            description: "A voxel model maker built into Minecraft. Allows players to create models by building them with blocks in the game and animate them ingame as well.",
+            stack: ['Java', 'Forge', 'ProGuard']
         },
         {
-            name: "Other project 1",
-            source: "somewhere",
-            description: "taaa daaaa",
-            stack: []
+            name: "RedZone MC",
+            source: "https://gitlab.com/PhoenixxFTW",
+            description: "A Minecraft 1.7.10 mod based off of Call of Duty and PUBG. Consists of multiple different game-modes such as TDM, FFA, and battle royale." +
+                " Mod as a built in match-making system, friends system, and much more.",
+            stack: ['Java', 'Kryonet', 'DiscordRPC']
         },
         {
-            name: "Other project 2",
-            source: "somewhere",
-            description: "taaa daaaa",
-            stack: []
+            name: "Impostors MC",
+            source: "https://github.com/PhoenixxFTW",
+            description: "A clone of the very popular game AmongUs, built into Minecraft. This plugin was made for the Socksfor1 server and was very popular for months.",
+            stack: ['Java', 'Spigot', 'Spring', 'Lombok']
+        },
+        {
+            name: "Phoenix Social Network",
+            source: "https://github.com/PhoenixxFTW/SocialNetworking",
+            description: "Basic Social Networking platform made for students using JavaFX for the frontend, Kryonet for the networking, and MySQL for data storage.",
+            stack: ['Java', 'Kryonet', 'JavaFX', 'MySQL']
         }
     ];
 
