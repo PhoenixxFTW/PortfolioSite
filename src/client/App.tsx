e1b8622d598c44ad0ce786ba01a7015ce24f9dbd
Phoenix <talpurjunaid5@gmail.com>
2023-06-23 15:37:06 -0400

    Attempting to get github pages working

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import './App.css';
 import { MainPage } from "./website/pages/MainPage";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import { ProjectView } from "./website/components/ProjectView";
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
         <Routes>
             <Route path="/" element={<MainPage />}/>
             <Route path="/ProjectView/:id" element={<ProjectView />}/>
         </Routes>
-    </Router>
+    </HashRouter>
   );
 }
 
